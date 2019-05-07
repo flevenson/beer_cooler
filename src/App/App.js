1a51643dddf8a47c81c2355229b1372042b587aa
Freddie Levenson <freddielev@gmail.com>
2019-05-07 13:56:47 -0600

    Begin Beer container file, changed App to Class Component instead of functional

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import BeerContainer from '../BeerContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  async componentDidMount(){
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="main-heading">Freddie's Beer Cooler</h1>
+        </header>
+        <main>
+          <BeerContainer />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/BeerContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BeerContainer.css'
+
+export const BeerContainer = (props) => {
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BeerContainer
